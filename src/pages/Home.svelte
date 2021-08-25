c6e7777e6a6ace9022e9d6be9082ce8bf90defae
Alex Iglesias <alexiglesias@finsweet.com>
2021-08-25 17:21:24 +0200

    Added EOL warning in the Homepage

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -18,12 +18,16 @@
   <section class="section">
     <Hero
       title="Advanced Webflow Forms"
-      subtitle={`Welcome to the <strong>beta</strong> version of the Advanced
+      subtitle={`
+      <strong>Important note:</strong>This project is no longer maintained in favor of the upcoming (and more advanced) Form Logic project that I'm working on in Finsweet. Feel free to play around with this app as much as you want, but I recommend you don't use it in serious projects as I won't be offering any support for it!
+      <br /><br />
+      Welcome to the <strong>beta</strong> version of the Advanced
       Forms Builder. Now you can add multi step functionality and conditional
       logic to your Webflow Forms!
       Demos and tutorials are comming soon, as well as new feature releases!`}
       primaryText="Get Started!"
       secondaryText="See Demos"
-      on:primaryclick={startBuilder} />
+      on:primaryclick={startBuilder}
+    />
   </section>
 </TransitionWrap>
